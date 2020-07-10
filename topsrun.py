b6346daa85f833eb31b5eaf860c80245221f141a
Mylene Jacquemart <mjacqu@gmail.com>
2020-07-10 17:22:33 -0600

    Adjusted to mirror changes to tops

--- topsrun.py
+++ topsrun.py
@@ -12,46 +12,53 @@ isce with 'topsApp.py --steps'.
 Use help(tops.Pair) and help(tops.makepairs) for details.
 '''
 
-data_path = '/net/tiampostorage/volume1/MyleneShare/data/bigsur_asc'
-#dates = [
-#    ('20151027', '20160119'),
-#    ('20151027', '20160212')]
-#maxdelta = datetime.timedelta(days = 48)
-singlemaster = datetime.datetime(2015, 10, 27)
+data_path = '/net/tiampostorage/volume1/MyleneShare/BarryArm/data/ascending/F194'
+dates = [('20200622', '20200704')]
+maxdelta = datetime.timedelta(days = 12)
+singleref = datetime.datetime(2017, 6, 26)
+sequential = 1
 
 # set dict with all attributes of pair:
 options = dict(
-    path = '/net/tiampostorage/volume1/MyleneShare/Bigsur_asc/az1rng2',
+    path = '/net/tiampostorage/volume1/MyleneShare/BarryArm/results/asc_194',
     swaths = [3],
-    orbit ='/net/tiampostorage/volume1/LabShare/orbits/aux_poeorb',
+    orbit ='/net/tiampostorage/volume1/LabShare/orbits/aux_resorb',
     auxiliary ='/home/myja3483/Landslides/Bigsur/auxfiles',
     unwrapper ='snaphu_mcf',
-    dem = '/net/tiampostorage/volume1/MyleneShare/10mdem/crop.dem.wgs84',
-    roi = [35.7622, 35.9909,-121.5001,-121.3209],
-    bbox = [35.7622, 35.9909,-121.5001,-121.3209],
+    dem = '/net/tiampostorage/volume1/MyleneShare/BarryArm/data/dem/stitched.dem.wgs84',
+    roi = [61.08453, 61.19802,-148.27746,-148.03239],
+    bbox = [61.08453, 61.19802,-148.27746,-148.03239],
     az_looks = 1,
-    rng_looks =2
+    rng_looks =3,
+    dense_offsets = False
     )
 
 
-# Example 1: run with no date specification
-#pairs = tops.make_pairs(path=data_path, options=options)
+# Example 1: make pairs with dates
+pairs = tops.make_pairs(path = data_path, dates = dates, options = options)
 
-# Example 2: run with maxdelta
-#pairs = tops.make_pairs(path=data_path, maxdelta  = maxdelta, options=options)
+# Example 2: make pairs with maxdelta
+pairs = tops.make_pairs(path=data_path, maxdelta  = maxdelta, options=options)
 
-# Example 3: run with singlemaster
-pairs = tops.make_pairs(path=data_path, singlemaster  = singlemaster, options=options)
+# Example 3: make pairs with singleref
+pairs = tops.make_pairs(path=data_path, singleref = singleref, options=options)
 
-# Example 4: run with dates
-#pairs = tops.make_pairs(path = data_path, dates = dates, options = options)
+# Example 4: make pairs with sequential pairing
+pairs = tops.make_pairs(path=data_path, sequential = 1, options=options)
 
+# Example 5: make pairs with any combination
+pairs = tops.make_pairs(path=data_path, dates = dates, maxdelta  = maxdelta, singleref = singleref, sequential = 1, options=options)
 
 for pair in pairs:
     print(pair.path)
 
-prin(len(pairs))
 
+print(len(pairs))
+
+# run ISCE pairs with overwrite
+#pair.run(overwrite = True)
+
+#run ISCE pairs without overwrite
 for pair in pairs:
     if os.path.isdir(str(pair.path)) == True:
         print(str(pair.path) + ' exists, skipping')
