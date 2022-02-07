ebbe06fdd5571e822df469b61db03882e24186b8
Mylene Jacquemart <mjacqu@gmail.com>
2022-02-07 11:24:46 +0100

    initial commit

---
+++ shadow_layover.py
@@ -0,0 +1,15 @@
+'''
+Outline:
+1. Create shadow and layover mask following Rees 2000
+2. Apply compression-factor analysis following Cigna 2014 et al.,
+
+Define geometric parameters:
+
+β: Terrain slope relative to horizontal
+α: Terrain aspect clockwise from north (or east?)
+γ: Satellite heading angle clockwise from north (or east?)
+θ: Angle between radar beam and vertical (look angle)
+_: Angle between radar beam and terrain normal (local incidence angle)
+
+
+'''
